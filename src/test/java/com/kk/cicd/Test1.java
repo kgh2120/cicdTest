3443964446e59f37ffd08cbad7ec63f988b0ba7f
kyoohyun <kgh2120@naver.com>
2023-02-02 16:15:37 +0900

    테스트 수정

--- src/test/java/com/kk/cicd/Test1.java
+++ src/test/java/com/kk/cicd/Test1.java
@@ -5,12 +5,6 @@ import org.junit.jupiter.api.Test;
 
 class Test1 {
 
-    @Test
-    void plus(){
-        int a = 10;
-        int b = 20;
 
-        Assertions.assertThat(20).isSameAs(a+b);
-    }
 
 }
